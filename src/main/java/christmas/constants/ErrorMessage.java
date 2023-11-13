63405de6e30923c4a80e88a2fb95c6a2c131cbf9
hgh1472 <hgh1472@naver.com>
2023-11-13 15:51:44 +0900

    feat : 상수로 사용되는 메세지 추가

---
+++ src/main/java/christmas/constants/ErrorMessage.java
@@ -0,0 +1,7 @@
+package christmas.constants;
+
+public class ErrorMessage {
+    public static final String DATE_ERROR_MESSAGE = "[ERROR] 유효하지 않는 날짜입니다. 다시 입력해 주세요.";
+    public static final String MENU_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+}
