740aa2046e18aa83e68ba774bf5c4db8b897da80
hgh1472 <hgh1472@naver.com>
2023-11-15 00:48:03 +0900

    feat : 이벤트 관련 상수 추가

--- src/main/java/christmas/constants/EventConstant.java
+++ src/main/java/christmas/constants/EventConstant.java
@@ -1,6 +1,7 @@
 package christmas.constants;
 
 public class EventConstant {
+    public static final int DISCOUNT_AVAILABLE_CHARGE = 10000;
     public static final String CHRISTMAS_D_DAY_DISCOUNT = "크리스마스 디데이 할인";
     public static final int D_DAY_DISCOUNT_START_MONEY = 1000;
     public static final int D_DAY_DISCOUNT_MONEY = 100;
@@ -12,4 +13,7 @@ public class EventConstant {
     public static final int SPECIAL_DISCOUNT_MONEY = 1000;
     public static final String GIFT_EVENT = "증정 이벤트";
     public static final int GIFT_EVENT_MINIMUM = 120000;
+    public static final String GIFT_MENU = "샴페인";
+    public static final int GIFT_MENU_COUNT = 1;
+    public static final boolean IS_GIFT = true;
 }
