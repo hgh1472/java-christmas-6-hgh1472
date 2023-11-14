35b8aeb55e3c59db972e7fc464b7d568ed9efe4e
hgh1472 <hgh1472@naver.com>
2023-11-14 21:29:07 +0900

    feat : 없음 메시지 추가

--- src/main/java/christmas/constants/OutputMessage.java
+++ src/main/java/christmas/constants/OutputMessage.java
@@ -1,12 +1,13 @@
 package christmas.constants;
 
 public class OutputMessage {
-    static final String EVENT_PREVIEW_MESSAGE = "우테코 식당에서 받을 이벤트 혜택 미리 보기!";
-    static final String MENULIST = "<주문 메뉴>";
-    static final String PRICE_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
-    static final String GIFT_MENU = "<증정 메뉴>";
-    static final String DISCOUNT_LIST = "<혜택 내역>";
-    static final String TOTAL_DISCOUNT = "<총혜택 금액>";
-    static final String EXPECTEDPAYMENT = "<할인 후 예상 결제 금액>";
-    static final String EVENTBADGE = "<12월 이벤트 배지>";
+    public static final String EVENT_PREVIEW_MESSAGE = "우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String MENULIST = "<주문 메뉴>";
+    public static final String PRICE_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+    public static final String GIFT_MENU = "<증정 메뉴>";
+    public static final String DISCOUNT_LIST = "<혜택 내역>";
+    public static final String TOTAL_DISCOUNT = "<총혜택 금액>";
+    public static final String EXPECTEDPAYMENT = "<할인 후 예상 결제 금액>";
+    public static final String EVENTBADGE = "<12월 이벤트 배지>";
+    public static final String NONE = "없음";
 }
