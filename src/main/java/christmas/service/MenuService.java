67c9cde14cc3da3774ae82cd28f2d3a58b59e5e1
hgh1472 <hgh1472@naver.com>
2023-11-15 01:07:06 +0900

    refactor : 메서드 주석 설명 추가

--- src/main/java/christmas/service/MenuService.java
+++ src/main/java/christmas/service/MenuService.java
@@ -8,10 +8,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MenuService {
+    // String으로 들어온 input(메뉴1-1,메뉴2-3...)을 ','기준으로 구분한 리스트를 반환합니다.
     public static List<String> parseMenu(String input) {
         return Arrays.asList(input.split(","));
     }
 
+    // ','를 기준으로 구분된 리스트의 하나의 메뉴에서 메뉴의 이름과 주문개수를 '-'로 구분합니다.
     public static List<String> getMenuInfo(String menuNameAndCount) {
         return Arrays.asList(menuNameAndCount.split("-"));
     }
