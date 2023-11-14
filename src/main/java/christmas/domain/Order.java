449b02be873e880f1d9c05ff77808529a782fcb6
hgh1472 <hgh1472@naver.com>
2023-11-14 22:11:59 +0900

    fix : 증정 메뉴 필드 추가에 따른 생성자 수정

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -4,6 +4,9 @@ import christmas.domain.badge.Badge;
 import christmas.domain.calendar.DecemberCalendar;
 import christmas.domain.menu.Menu;
 import christmas.domain.menu.MenuList;
+import christmas.domain.menusheet.DrinkList;
+import christmas.validator.MenuValidator;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
@@ -11,15 +14,38 @@ public class Order {
     MenuList menuList;
     Price price;
     Badge badge;
+    MenuList giftList;
 
-    Order(int date, List<String> menu) {
-        this.dayInfo = DecemberCalendar.getDayInfo(date);
-        for (String menuName : menu) {
-            menuList.addMenu(Menu.orderMenu(menuName));
-        }
+    private Order(DecemberCalendar dayInfo, MenuList menuList) {
+        this.dayInfo = dayInfo;
+        this.menuList = menuList;
         this.price = Price.makePrice(this.dayInfo, this.menuList);
-        this.badge = Badge.makeBadge(price.getBeforeDiscountPrice());
+        List<Menu> giftList = new ArrayList<>();
+        if (MenuValidator.canReceiveGift(this.price)) {
+            Menu gift = Menu.orderMenu(DrinkList.Champagne.getMenuName(), 1);
+            giftList.add(gift);
+        }
+        this.giftList = MenuList.makeGiftList(giftList);
+        this.badge = Badge.makeBadge(price.calculateTotalDiscount());
     }
 
+    public static Order makeOrder(DecemberCalendar dayInfo, MenuList menuList) {
+        return new Order(dayInfo, menuList);
+    }
 
+    public MenuList getMenuList() {
+        return this.menuList;
+    }
+
+    public Price getPrice() {
+        return this.price;
+    }
+
+    public MenuList getGiftList() {
+        return this.giftList;
+    }
+
+    public Badge getBadge() {
+        return this.badge;
+    }
 }
