a8895d5654458e8e9cc7e12afc7d3773b0dd3165
hgh1472 <hgh1472@naver.com>
2023-11-14 21:30:06 +0900

    feat : 배지 상수 추가

---
+++ src/main/java/christmas/constants/BadgeList.java
@@ -0,0 +1,7 @@
+package christmas.constants;
+
+public class BadgeList {
+    public static final String StarBadge = "별";
+    public static final String TreeBadge = "트리";
+    public static final String SantaBadge = "산타";
+}
